17eb12a5afdf227bdfe71c4a37f42928ded0c03c
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-04-28 18:03:11 +0900

    feat: add logo

---
+++ .storybook/manager.js
@@ -0,0 +1,8 @@
+import {addons} from '@storybook/addons'
+import {create} from '@storybook/theming/create'
+
+const theme = create({
+  base:'light',
+  brandTitle: 'react-native-sketchbook',
+  brandImage: 'https://user-images.githubusercontent.com/6936373/80468413-1cc66180-897a-11ea-9965-177ca6e186d1.png'
+})
