a36e34cdcbf981df89126ca898d1fbf34a3a2676
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-05-05 15:20:55 +0900

    chore: allow custom theme

--- src/Themes/theme.ts
+++ src/Themes/theme.ts
@@ -1,8 +1,16 @@
-import {DefaultColorTheme} from './colors';
-import {DefaultFontSize, DefaultSpaceSize} from './size';
+import {DefaultColorTheme, Color} from './colors';
+import {DefaultFontSize, DefaultSpaceSize, FontSize, SpaceSize} from './size';
 
-export const DefaultTheme = {
+export type Theme = {
+  colors: Record<'light' | 'dark', Colors>;
+  fontSizes: Record<FontSize, number>;
+  spaceSizes: Record<SpaceSize, number>;
+};
+
+export const DefaultTheme: Theme = {
   colors: DefaultColorTheme,
   fontSizes: DefaultFontSize,
   spaceSizes: DefaultSpaceSize,
 } as const;
+
+type Colors = Record<Color, string>;
