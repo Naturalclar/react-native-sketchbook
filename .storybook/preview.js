542923286bce6d6492543b24d012e6125fd8dc55
Jesse Katsumata <niconico.clarinet@gmail.com>
2020-10-15 02:23:41 +0900

    chore: update storybook config (#39)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,7 @@
-import {configure, addParameters, addDecorator} from '@storybook/react';
+import {addParameters, addDecorator} from '@storybook/react';
 import {withKnobs} from '@storybook/addon-knobs';
 import {INITIAL_VIEWPORTS} from '@storybook/addon-viewport';
 import {StoryDecorator} from './StoryDecorator';
-
-const loadStories = () => {
-  const req = require.context('../src', true, /\.stories\.tsx$/);
-  return req.keys().map((fname) => req(fname));
-};
 
 addDecorator(
   withKnobs({
@@ -26,5 +21,3 @@ addParameters({
     defaultViewport: 'iphone6',
   },
 });
-
-configure(loadStories, module);
