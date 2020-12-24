7614e7a72627f2543f2e85a45cdfb28f11634e11
Jesse Katsumata <niconico.clarinet@gmail.com>
2020-12-24 19:11:13 +0900

    feat: add util props for flex and row (#49)

--- src/Utils/Flex.tsx
+++ src/Utils/Flex.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import {View, StyleSheet, ViewProps} from 'react-native';
 
-const styles = StyleSheet.create({
-  flex: {flex: 1},
-});
+type Props = {
+  flex?: number;
+} & ViewProps;
 
 /**
  * Flex
  * - A View Component with default style being flex:1
  */
-export const Flex: React.FC<ViewProps> = ({style, ...rest}) => (
-  <View style={[styles.flex, StyleSheet.flatten(style)]} {...rest} />
+export const Flex: React.FC<Props> = ({style, flex = 1, ...rest}) => (
+  <View style={[{flex}, StyleSheet.flatten(style)]} {...rest} />
 );
--- src/Utils/Row.tsx
+++ src/Utils/Row.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, StyleSheet, ViewProps} from 'react-native';
+import {View, StyleSheet, ViewProps, FlexStyle} from 'react-native';
 
 const styles = StyleSheet.create({
   default: {
@@ -7,10 +7,31 @@ const styles = StyleSheet.create({
   },
 });
 
+type Props = {
+  alignItems?: FlexStyle['alignItems'];
+  justifyContent?: FlexStyle['justifyContent'];
+  alignContent?: FlexStyle['alignContent'];
+} & ViewProps;
+
 /**
  * Row
  * - A View Component whose default flexDiretion is row
  */
-export const Row: React.FC<ViewProps> = ({style, ...rest}) => {
-  return <View style={[styles.default, StyleSheet.flatten(style)]} {...rest} />;
+export const Row: React.FC<Props> = ({
+  style,
+  alignItems,
+  alignContent,
+  justifyContent,
+  ...rest
+}) => {
+  return (
+    <View
+      style={[
+        styles.default,
+        {alignItems, alignContent, justifyContent},
+        StyleSheet.flatten(style),
+      ]}
+      {...rest}
+    />
+  );
 };
