6890474ecb9ffab117afd17b1eb6bd6e740bac2f
Jesse Katsumata <jesse.katsumata@gmail.com>
2021-02-03 17:51:36 +0900

    feat: add flex style props

--- src/Utils/Flex.tsx
+++ src/Utils/Flex.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
-import {View, StyleSheet, ViewProps} from 'react-native';
+import {View, StyleSheet, ViewProps, FlexStyle} from 'react-native';
 
 type Props = {
   flex?: number;
+  alignItems?: FlexStyle['alignItems'];
+  justifyContent?: FlexStyle['justifyContent'];
 } & ViewProps;
 
 /**
  * Flex
  * - A View Component with default style being flex:1
  */
-export const Flex: React.FC<Props> = ({style, flex = 1, ...rest}) => (
-  <View style={[{flex}, StyleSheet.flatten(style)]} {...rest} />
+export const Flex: React.FC<Props> = ({
+  style,
+  flex = 1,
+  alignItems,
+  justifyContent,
+  ...rest
+}) => (
+  <View
+    style={[{flex, alignItems, justifyContent}, StyleSheet.flatten(style)]}
+    {...rest}
+  />
 );
