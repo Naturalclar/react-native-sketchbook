7d827397b594a149758ee5114b25914a620df480
Jesse Katsumata <niconico.clarinet@gmail.com>
2021-06-11 17:45:25 +0900

    feat: add Avatar (#157)

---
+++ src/Avatars/AvatarGroup.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {Row} from '../Utils/Row';
+type AvatarGroupProps = {
+  space?: number;
+};
+
+/**
+ * Shows Group of Avatars, slightly stacked on top of each other
+ *
+ * ```js
+ * import * as React from 'react'
+ * import { Avatar, AvatarGroup } from 'react-native-sketchbook'
+ *
+ * export const Example = () => {
+ *   return (
+ *     <AvatarGroup>
+ *       <Avatar size={50} source={require('../assets/example1.png')}/>
+ *       <Avatar size={50} source={require('../assets/example2.png')}/>
+ *       <Avatar size={50} source={require('../assets/example3.png')}/>
+ *       <Avatar size={50} source={require('../assets/example4.png')}/>
+ *     </AvatarGroup>
+ *   )
+ * }
+ * ```
+ */
+export const AvatarGroup: React.FC<AvatarGroupProps> = ({
+  children,
+  space = -8,
+}) => {
+  const childElements = React.Children.toArray(children).filter((child) => {
+    return React.isValidElement(child);
+  });
+
+  return (
+    <Row style={{paddingLeft: -space}}>
+      {childElements.map((child) => {
+        return <View style={{marginLeft: space}}>{child}</View>;
+      })}
+    </Row>
+  );
+};
---
+++ src/Avatars/index.ts
@@ -0,0 +1,2 @@
+export {Avatar} from './Avatar';
+export {AvatarGroup} from './AvatarGroup';
