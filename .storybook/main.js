17455482a42e604b9631487d813281dcf87c2675
Jesse Katsumata <niconico.clarinet@gmail.com>
2021-07-03 22:02:40 +0900

    chore: update storybook config (#170)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
+const externalLibs = [path.resolve(__dirname, '../node_modules/react-native')];
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
@@ -6,4 +10,39 @@ module.exports = {
     '@storybook/addon-knobs',
     '@storybook/addon-viewport',
   ],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-expo'],
+          },
+        },
+      ],
+    });
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      include: externalLibs,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-expo'],
+          },
+        },
+      ],
+    });
+
+    config.resolve.alias = {
+      // replace `react-native` imports with `react-native-web`
+      'react-native$': require.resolve('react-native-web'),
+    };
+
+    config.resolve.extensions.push('.ts', '.tsx', '.web.js', '.web.tsx');
+
+    return config;
+  },
 };
